30ff7ac403589dd9ede428a623e5fb849487ccef
Michelle Romero <brianromero3012@gmail.com>
2025-04-22 21:51:36 -0600

    automatizacion para los modulos de roles y permisos de los usuarios

--- bancoSangre/donador/api/views.py
+++ bancoSangre/donador/api/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets
 from donador.models import Donador
 from donador.api.serializer import DonadorSerializer
-from usuario.api.permissions import IsDonadorCreateReadOnly
+from usuario.api.permissions import IsDonadorCreateReadOnly  # Asegúrate de importar el permiso
 
 class DonadorViewSet(viewsets.ModelViewSet):
-    queryset = Donador.objects.all()
-    serializer_class = DonadorSerializer
-    permission_classes = [IsDonadorCreateReadOnly]
+    queryset = Donador.objects.all()  # Todos los donadores
+    serializer_class = DonadorSerializer  # El serializador de los donadores
+    permission_classes = [IsDonadorCreateReadOnly]  # Asigna el permiso aquí para controlar el acceso
